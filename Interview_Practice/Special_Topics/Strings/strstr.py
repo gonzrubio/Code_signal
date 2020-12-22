6f7f21191be914d95713cfb6ea1aad8bb87a4afe
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-12-22 12:04:22 -0800

    feat (strstr): Upload Robin-karp Naive algorithm and fast look up table solution.

--- Interview_Practice/Special_Topics/Strings/strstr.py
+++ Interview_Practice/Special_Topics/Strings/strstr.py
@@ -6,17 +6,51 @@ in the text of the first occurrence of the pattern.
 
 
 def strstr(text, pattern):
-    """Needle in haystack O(|text|) time? and O(?) space.
+    """Needle in haystack.
 
     Return an integer indicating the first occurrence of pattern in the text,
     or -1 if the pattern is not part of the text.
     """
-    lookup = set([pattern])
-    len_text = len(text)
-    len_pattern = len(pattern)
-    for index in range(len_text-len_pattern+1):
-        if text[index:index+len_pattern] in lookup:
-            return index
+    # pattern_hash = hash(pattern)
+    # len_text = len(text)
+    # len_pattern = len(pattern)
+    # for index in range(len_text-len_pattern+1):
+    #     text_hash = hash(text[index:index+len_pattern])
+    #     if pattern_hash != text_hash:
+    #         continue
+    #     if text[index:index+len_pattern] == pattern:
+    #         return index
+    if not pattern or not text:
+        return -1
+
+    # First build the pattern lookup table
+    tbl = [0] * (1 + len(pattern))
+    i = 1
+    j = 0
+    while i < len(pattern):
+        if pattern[i] == pattern[j]:
+            i += 1
+            j += 1
+            tbl[i] = j
+        elif 0 == j:
+            i += 1
+        else:
+            j = tbl[j]
+
+    # Search over the query text
+    i = 0
+    j = 0
+    while i < len(text):
+        if text[i] == pattern[j]:
+            i += 1
+            j += 1
+            if len(pattern) == j:
+                assert(text[i - len(pattern): i] == pattern)
+                return i - len(pattern)
+        elif j == 0:
+            i += 1
+        else:
+            j = tbl[j]
     return -1
 
 
@@ -35,9 +69,3 @@ def tests():
 
 if __name__ == "__main__":
     tests()
-
-# Options
-# str.find() source code
-# Implement hash() with robin-algorithm
-# best solution in codesignal
-# Code review
